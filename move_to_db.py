eb19ead1ca27bc6cc45f19b9a58bf95af066376b
Alex2000prof <sashapribylov19@gmail.com>
2024-08-24 22:41:08 +0300

    move_to_db.py
    Update

--- move_to_db.py
+++ move_to_db.py
@@ -1,38 +1,42 @@
 import psycopg2
 import csv
+
 data_base = {
-    'dbname': 'hackathon',
-    'user': 'postgres',
-    'password': '1739',
-    'host': 'localhost',
+    'dbname': 'postgres',
+    'user': 'priib',
+    'password': 'vbigHwNPR5r2TBumP1x5liRKLooAShQ7',
+    'host': 'dpg-cr53675umphs73e05b10-a',
     'port': '5432'
 }
 
- 
-def add_to_db_recipe(csv_file,data_base):
-    try: 
+def add_to_db_recipe(csv_file, data_base):
+    try:
         connection = psycopg2.connect(**data_base)
         cursor = connection.cursor()
         
-        with open(csv_file,'r') as file:
+        with open(csv_file, 'r') as file:
             read = csv.reader(file)
             for row in read:
                 cursor.execute('''
-INSERT INTO all_info_from_csv (food_type, cuisine, allergies, specific_dish, recipe) VALUES (%s,%s,%s,%s,%s)
-''',row)
+                    INSERT INTO all_info_from_csv (food_type, cuisine, allergies, specific_dish, recipe)
+                    VALUES (%s, %s, %s, %s, %s)
+                ''', row)
         connection.commit()
-        print("Data loaded.")
+        print("Data loaded successfully.")
     except Exception as e:
-        print(f"Error{e}")
-
+        print(f"Error while loading data: {e}")
     finally:
-        cursor.close()
-        connection.close()
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
 
-add_to_db_recipe('preferences.csv',data_base)
 def save_csv(data):
-    CSV_FILE = 'preferences.csv' 
-    with open(CSV_FILE,mode = 'a',newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(data)
-    add_to_db_recipe(CSV_FILE,data_base)
+    CSV_FILE = 'preferences.csv'
+    try:
+        with open(CSV_FILE, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+        add_to_db_recipe(CSV_FILE, data_base)
+    except Exception as e:
+        print(f"Error while saving CSV: {e}")
