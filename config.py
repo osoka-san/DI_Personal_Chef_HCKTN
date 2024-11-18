2bc2652056f354d5d6dacd8b49b320670d3be6c4
Aleksander Pribylov <sashapribylov19@gmail.com>
2024-11-18 20:12:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 load_dotenv()
 class Config:
     # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    OPENAI_API_KEY = "sk-svcacct-7RKvYv2IOwtYu25Z4mYGN8FE1ujsti5jemKuCNa6FVz-6DDyRVhITWpSMKw6g3cMT3BlbkFJla3-v60CWHOwazE_3uWQ69E0CILKAxItZwwjz2Ntn92lZpZPV-5XRoQO-eCp2o4A"
+    OPENAI_API_KEY = "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"
 
     DEFAULT_LANGUAGE = os.getenv("DEFAULT_LANGUAGE", "en")
     MAX_TOKENS = int(os.getenv("MAX_TOKENS", 1000))
